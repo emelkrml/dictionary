119f2abafb90ae6540d3cc698710c6ff4a6c3583
Your Name <you@example.com>
2016-09-02 18:00:14 +0300

    refs #8: Kategoriye gore baslikların getirilmesi

---
+++ dictionary2/topics/templatetags/topic_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+from dictionary2.topics.models import Category
+register = template.Library()
+
+@register.assignment_tag
+def get_categories():
+    category_list = Category.objects.all()
+    return category_list
+
--- dictionary2/topics/views.py
+++ dictionary2/topics/views.py
@@ -2,12 +2,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.db.models import Count
+from django.template import RequestContext
+
 from dictionary2.topics.models import Entry, Topic, Category, Favorite
+
 
 
 def entry_view(request):
     category = Category.objects.first()
-    topics = Topic.objects.all().prefetch_related('entry_set')[6:11]
+    topics = Topic.objects.all().prefetch_related('entry_set')[10:]
     populer = Topic.objects.annotate(entry_count=Count('entry')).order_by('-entry_count')
     context = {
         'topics': topics,
@@ -15,6 +18,17 @@ def entry_view(request):
         'populer': populer,
     }
     return render(request, 'topics/title.html', context)
+
+
+def entry(request):
+    populer = Topic.objects.annotate(entry_count=Count('entry')).order_by('-entry_count')
+    topics = Topic.objects.get(entry__content='entry')
+    context = {
+        'populer': populer,
+        'topics': topics,
+    }
+    return render(request, 'topics/entry.html', context)
+
 
 def today(request):
     try:
@@ -27,7 +41,8 @@ def today(request):
             'today': None,
         }
 
-    return render(request, 'topics/kanallar/bugun.html', context)
+    return render(request, 'topics/bugun.html', context)
+
 
 def populer(request):
     try:
@@ -37,30 +52,59 @@ def populer(request):
             'populer': populer,
         }
     except:
+        return render(request, 'topics/title.html')
+
+    return render(request, 'topics/populer.html', context)
+
+
+def caylak(request):
+    try:
+        caylak = Topic.objects.filter(user__junior=1)
+        context = {
+            'caylak': caylak,
+        }
+    except:
         return render(request, 'base2.html')
 
-    return render(request, 'topics/kanallar/populer.html', context)
+    return render(request, 'topics/caylaklar.html', context)
 
 
-def by_category(request, category):
+def by_category(request, category_id):
 
-    category = get_object_or_404(Category, pk=category)
+    topics = Topic.objects.filter(category__id=category_id).prefetch_related('entry_set')
+    context = {
+        'topics': topics,
+    }
 
-    topics = Topic.objects.filter(category=category).prefetch_related('entry_set')
+    return render(request, 'topics/by_category.html', context)
 
-    return render(request, 'topics/kanallar/by_category.html')
 
-def basiboslar(request, category):
-    category = get_object_or_404(Category, pk=category)
-
+def basiboslar(request):
     topics = Topic.objects.filter(category=None).prefetch_related('entry_set')
+    context = {
+        'topics': topics,
+    }
 
-    return render(request, 'topics/kanallar/basiboslar.html')
+    return render(request, 'topics/basiboslar.html', context)
 
 
+'''
 def favorite(request):
-    pass
-#http://www.tangowithdjango.com/book/chapters/ajax.html
+    context = RequestContext(request)
+    entry_id = None
+    if request.method == 'GET':
+        e_id = request.GET['entry_id']
+
+    fav = 0
+    if e_id:
+        entry = Entry.objects.get(id=int(e_id))
+        if entry:
+            fav = entry.fav + 1
+            entry.fav = fav
+            entry.save()
+
+    return HttpResponse(fav)
+'''
 
 
 
